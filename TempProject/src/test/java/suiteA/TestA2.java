87691353f57b9821bbc456dbe1f69818fa47bc26
ashishselenium1 <appiumselendroid@gmail.com>
2018-04-13 11:29:56 +0530

    temp proj

---
+++ TempProject/src/test/java/suiteA/TestA2.java
@@ -0,0 +1,14 @@
+package suiteA;
+
+import org.testng.annotations.Test;
+
+public class TestA2 {
+
+	
+	@Test
+	public void testA2() throws InterruptedException {
+		System.out.println("Starting A2");
+		Thread.sleep(5000);
+		System.out.println("Ending A2");
+	}
+}
---
+++ TempProject/src/test/java/suiteB/TestB2.java
@@ -0,0 +1,13 @@
+package suiteB;
+
+import org.testng.annotations.Test;
+
+public class TestB2 {
+
+	@Test
+	public void testB2() throws InterruptedException {
+		System.out.println("Starting B2");
+		Thread.sleep(5000);
+		System.out.println("Ending B2");
+	}
+}
